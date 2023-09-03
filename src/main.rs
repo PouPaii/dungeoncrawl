7c3b3bdbe8f32e42af1fde8c197d269429d98e8d
Pia Hertenstein <pschlr@web.de>
2023-09-03 15:06:08 +0200

    [update] using legion to manage graphics

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 use bracket_lib::prelude::*;
 
+mod systems;
 mod map;
 mod map_builder;
 mod camera;
+mod components;
+mod spawner;
 mod prelude { 
     pub use bracket_lib::prelude::*;
     pub use legion::*;
@@ -13,23 +16,33 @@ mod prelude {
     pub const DISPLAY_WIDTH: i32 = SCREEN_WIDTH/2;
     pub const DISPLAY_HEIGHT: i32 = SCREEN_HEIGHT/2;
     pub const TILE_SIZE: i32 = 32;
+    pub use crate::systems::*;
     pub use crate::map::*;
     pub use crate::map_builder::*;
     pub use crate::camera::*;
+    pub use crate::components::*;
+    pub use crate::spawner::*;
 }
 use prelude::*;
 
 struct State {
-    map: Map,
-    camera: Camera,
+    ecs : World,
+    resources : Resources,
+    systems : Schedule,
 }
 impl State {
     fn new() -> Self {
+        let mut ecs = World::default();
+        let mut resources = Resources::default();
         let mut rng = RandomNumberGenerator::new();
         let map_builder = MapBuilder::new(&mut rng);
+        spawn_player(&mut ecs, map_builder.player_start);
+        resources.insert(map_builder.map);
+        resources.insert(Camera::new(map_builder.player_start));
         Self {
-            map : map_builder.map,
-            camera : Camera::new(map_builder.player_start),
+            ecs,
+            resources,
+            systems: build_scheduler()
         }
     }
 }
@@ -39,8 +52,9 @@ impl GameState for State {
         ctx.cls();
         ctx.set_active_console(1);
         ctx.cls();
-        //TODO: Execute Systems
-        //TODO: Render Draw Buffer
+        self.resources.insert(ctx.key);
+        self.systems.execute(&mut self.ecs, &mut self.resources);
+        render_draw_buffer(ctx).expect("Render error");
     }
 }
 
